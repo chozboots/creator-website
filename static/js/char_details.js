65e410b0fb00041750a12a0e2c642cf61ab9cc87
choz <bharlescooth@gmail.com>
2023-11-09 01:22:46 -0600

    first varies menu

--- static/js/char_details.js
+++ static/js/char_details.js
@@ -281,6 +281,228 @@ document.addEventListener('DOMContentLoaded', () => {
 
             return heightWrapper;
 
+        } else if (element.measurement === 'size') {
+            // Create a wrapper for all length related elements
+            const lengthWrapper = document.createElement('div');
+            lengthWrapper.className = 'length-input-wrapper';
+        
+            const lengthDisplayContainer = document.createElement('div');
+            lengthDisplayContainer.className = 'length-display-container';
+        
+            const lengthDisplaySpan = document.createElement('span');
+            lengthDisplaySpan.className = 'length-display';
+            lengthDisplayContainer.appendChild(lengthDisplaySpan);
+            lengthWrapper.appendChild(lengthDisplayContainer);
+        
+            // Create the dropdown menu for selecting mode
+            const modeSelect = document.createElement('select');
+            modeSelect.className = 'mode-select'; // Class for styling
+            ['Numeric', 'Varies', 'None'].forEach(mode => {
+                const option = document.createElement('option');
+                option.value = mode;
+                option.textContent = mode;
+                modeSelect.appendChild(option);
+            });
+        
+            // Append the dropdown to the lengthWrapper
+            lengthWrapper.appendChild(modeSelect);
+        
+            // Create the number input for cm
+            const cmInput = document.createElement('input');
+            Object.assign(cmInput, {
+                type: 'number',
+                name: element.name + 'Cm',
+                placeholder: 'Centimeters',
+                className: 'length-input-cm', // Class for styling
+                min: 0
+            });
+            lengthWrapper.appendChild(cmInput);
+        
+            // Create the number input for inches
+            const inchInput = document.createElement('input');
+            Object.assign(inchInput, {
+                type: 'number',
+                name: element.name + 'Inch',
+                placeholder: 'Inches',
+                className: 'length-input-inch', // Class for styling
+                min: 0
+            });
+            lengthWrapper.appendChild(inchInput);
+        
+            // Function to toggle visibility of inputs and display based on mode
+            const toggleInputsVisibility = (mode) => {
+                const isNumericMode = mode === 'Numeric';
+                // Show inputs only in Numeric mode
+                cmInput.style.display = isNumericMode ? '' : 'none';
+                inchInput.style.display = isNumericMode ? '' : 'none';
+                // Hide display for Varies and None mode
+                lengthDisplayContainer.style.display = isNumericMode ? '' : 'none';
+            };
+        
+            // Event listener for the dropdown menu to change mode
+            modeSelect.addEventListener('change', () => {
+                const selectedMode = modeSelect.value;
+                toggleInputsVisibility(selectedMode);
+            });
+        
+            // Initialize with the default mode
+            toggleInputsVisibility(modeSelect.value);
+        
+            const cmToInch = (cm) => {
+                const inches = cm / 2.54;
+                return inches;
+            };
+        
+            const inchToCm = (inches) => {
+                const cm = inches * 2.54;
+                return cm;
+            };
+        
+            const updateLengthDisplay = (cm, inches) => {
+                // Only update the display in Numeric mode
+                if (modeSelect.value === 'Numeric') {
+                    lengthDisplaySpan.textContent = `${inches.toFixed(1)}" (${cm.toFixed(1)} cm)`;
+                }
+            };
+        
+            // Event listener for the cm input
+            cmInput.addEventListener('input', () => {
+                const cm = parseFloat(cmInput.value);
+                const inches = cmToInch(cm);
+                inchInput.value = inches.toFixed(1);
+                updateLengthDisplay(cm, inches);
+            });
+        
+            // Event listener for the inches input
+            inchInput.addEventListener('input', () => {
+                const inches = parseFloat(inchInput.value);
+                const cm = inchToCm(inches);
+                cmInput.value = cm.toFixed(1);
+                updateLengthDisplay(cm, inches);
+            });
+        
+            // Set the default value for cm and inches input and update the display
+            const defaultCmValue = 0;
+            cmInput.value = defaultCmValue;
+            const defaultInchesValue = cmToInch(defaultCmValue);
+            inchInput.value = defaultInchesValue.toFixed(1);
+            updateLengthDisplay(defaultCmValue, defaultInchesValue);
+        
+            // Return the lengthWrapper containing all elements
+            return lengthWrapper;
+        
+        } else if (element.list_type === 'dynamic_list') {
+            const listWrapper = document.createElement('div');
+            listWrapper.className = 'dynamic-list-wrapper';
+    
+            const list = document.createElement('ul');
+            list.className = 'dynamic-list';
+    
+            const addButton = document.createElement('button');
+            addButton.textContent = 'Add';
+            addButton.type = 'button';
+            addButton.className = 'add-button';
+            addButton.onclick = () => addListItem(list, element.item_name);
+    
+            // Function to add list item
+            function addListItem(list, itemName) {
+                const listItem = document.createElement('li');
+                const input = document.createElement('input');
+                input.type = 'text';
+                input.name = itemName;
+    
+                const removeButton = document.createElement('button');
+                removeButton.textContent = 'Remove';
+                removeButton.type = 'button';
+                removeButton.className = 'remove-button';
+                removeButton.onclick = () => list.removeChild(listItem);
+    
+                listItem.appendChild(input);
+                listItem.appendChild(removeButton);
+                list.appendChild(listItem);
+            }
+    
+            // Initial item
+            addListItem(list, element.item_name);
+    
+            listWrapper.appendChild(list);
+            listWrapper.appendChild(addButton);
+    
+            return listWrapper;
+        } else if (element.list_type === 'dynamic_list_with_titles') {
+            const listWrapperWithTitles = document.createElement('div');
+            listWrapperWithTitles.className = 'dynamic-list-wrapper';
+        
+            const listWithTitles = document.createElement('ul'); // Changed variable name here
+            listWithTitles.className = 'dynamic-list-with-titles'; // Assign the class to the list
+        
+            const addButton = document.createElement('button');
+            addButton.textContent = 'Add';
+            addButton.type = 'button';
+            addButton.className = 'add-button';
+            addButton.onclick = () => addListItem(listWithTitles, element.item_name, element.default_title, element.default_description);
+        
+            function addListItem(list, itemName, defaultTitle = '', defaultDescription = '', maxlengthTitle = 255, maxlengthDesc = 1000) {
+                const listItem = document.createElement('li');
+                listItem.className = 'dynamic-list-with-titles-item';
+            
+                // Title input
+                const title = document.createElement('input');
+                const titleCounter = document.createElement('span');
+                title.type = 'text';
+                title.value = defaultTitle;
+                title.name = `${itemName}-title`;
+                title.className = 'title-input';
+                title.maxLength = maxlengthTitle;
+                titleCounter.className = 'char-counter';
+                titleCounter.textContent = `0 / ${maxlengthTitle}`;
+            
+                // Update counter for the title input
+                title.addEventListener('input', () => {
+                    titleCounter.textContent = `${title.value.length} / ${maxlengthTitle}`;
+                });
+            
+                // Description textarea
+                const description = document.createElement('textarea');
+                const descCounter = document.createElement('span');
+                description.value = defaultDescription;
+                description.name = `${itemName}-description`;
+                description.className = 'description-textarea';
+                description.rows = 4;
+                description.maxLength = maxlengthDesc;
+                descCounter.className = 'char-counter';
+                descCounter.textContent = `0 / ${maxlengthDesc}`;
+            
+                // Update counter for the description textarea
+                description.addEventListener('input', () => {
+                    descCounter.textContent = `${description.value.length} / ${maxlengthDesc}`;
+                });
+            
+                // Remove button
+                const removeButton = document.createElement('button');
+                removeButton.textContent = 'Remove';
+                removeButton.type = 'button';
+                removeButton.className = 'remove-button';
+                removeButton.onclick = () => list.removeChild(listItem);
+            
+                // Append elements to the listItem
+                listItem.appendChild(title);
+                listItem.appendChild(titleCounter);
+                listItem.appendChild(description);
+                listItem.appendChild(descCounter);
+                listItem.appendChild(removeButton);
+            
+                list.appendChild(listItem);
+            }            
+            
+            if (element.default_title || element.default_description) {
+                addListItem(listWithTitles, element.item_name, element.default_title, element.default_description);
+            }
+        
+            listWrapperWithTitles.appendChild(listWithTitles);
+            listWrapperWithTitles.appendChild(addButton);
+        
+            return listWrapperWithTitles;
         } else {
             // For all other cases, continue with the original logic
             const inputType = element.type === 'dropdown' ? 'select' : 'input';
